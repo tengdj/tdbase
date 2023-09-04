0850e7ad82a67daedacbd01784107ae7dfc211da
tengdj <tengdj@vip.qq.com>
2023-09-04 21:06:51 +0800

    minor update

--- src/geometry/computing_gpu.cu
+++ src/geometry/computing_gpu.cu
@@ -428,13 +428,13 @@ void TriDist_cuda(const float *data, const uint *offset_size, result_container *
 	}
 }
 
-__device__ static float atomicMax(float* address, float val) {
+__device__ static float atomicMin(float* address, float val) {
     int* address_as_i = (int*) address;
     int old = *address_as_i, assumed;
     do {
         assumed = old;
         old = ::atomicCAS(address_as_i, assumed,
-            __float_as_int(::fmaxf(val, __int_as_float(assumed))));
+            __float_as_int(::fminf(val, __int_as_float(assumed))));
     } while (assumed != old);
     return __int_as_float(old);
 }
